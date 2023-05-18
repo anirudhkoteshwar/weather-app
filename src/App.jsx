61a267a01a8c4ccee11214fd2570c122b6e0cedd
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2023-05-18 12:28:24 +0530

    working on fetchapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import './App.css'
-import Search from './components/search/search'
+import "./App.css";
+import Search from "./components/search/search";
+import { useState } from "react";
 
 function App() {
 
+  const handleSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+  }
+
   return (
-    <div className='panelWrapper'>
-      <div className='leftPanel'>
-        hi
-      </div>
-      <div className='rightPanel'>
-        <Search />
+    <div className="panelWrapper">
+      <div className="leftPanel"></div>
+      <div className="rightPanel">
+        <Search onSearchChange={handleSearchChange} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,15 +1,46 @@
-import searchIcon from '/icons/searchIcon.svg';
-import './search.css';
+import searchIcon from "/icons/searchIcon.svg";
+import "./search.css";
+import { useState } from "react";
+import { GEODB_OPTIONS, GEODB_URL } from "../APIs/geodb";
 
-function Search() {
-    return ( 
-        <div className='searchBar'>
-            <input type='text' placeholder='Enter City Name' id='citySearch' />
-            <div className='searchIcon'>
-                <img src={searchIcon}  alt="Search Icon"/>
-            </div>
+function Search({ onSearchChange }) {
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const getCities = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEODB_URL}?namePrefix=${inputValue}`, GEODB_OPTIONS );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearchValue(searchData);
+    onSearchChange(searchData);
+  }
+
+  return (
+    <>
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Enter City Name"
+          id="citySearch"
+          value={searchValue}
+          onChange={handleOnChange}
+          
+        />
+        <div className="searchIcon">
+          <img src={searchIcon} alt="Search Icon" />
         </div>
-     );
+      </div>
+      <div className="dataResults"></div>
+    </>
+  );
 }
 
 export default Search;
