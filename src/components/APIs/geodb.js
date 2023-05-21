244f1b49819a03aa2328c822d706e37d4d282d9f
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2023-05-21 21:27:27 +0530

    debounce search works

--- src/components/APIs/geodb.js
+++ src/components/APIs/geodb.js
@@ -2,7 +2,7 @@ export const GEODB_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const GEODB_OPTIONS = {
   method: 'GET',
   headers: {
-      'X-RapidAPI-Key': 'ec7bbde42emsh8a5c40c168ca83cp10953djsncf6cd944212c',
+      'X-RapidAPI-Key': `${import.meta.env.VITE_GEODB_API_KEY}`,
       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
   }
 };
